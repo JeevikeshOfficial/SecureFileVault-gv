b1f65fde3ed25e714a1c465fd6d7662870a169df
Jeevikesh S <jeevikesh.official@gmail.com>
2025-07-12 11:41:29 +0530

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,9 +1,9 @@
 export const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: '**********_********',
-      userPoolClientId: '***********************',
-      identityPoolId: '**********:****-****-****-****-************',
+      userPoolId: 'ap-south-1_je8DiVCvL',
+      userPoolClientId: '63d4vusd967v6iueug26974e3p',
+      identityPoolId: 'ap-south-1:f8daa265-0d2c-4991-8280-cb645457077e',
       loginWith: {
         email: true,
         username: true
@@ -12,8 +12,8 @@ export const awsConfig = {
   },
   Storage: {
     S3: {
-      bucket: 'YOUR-BUCKET-NAME',
-      region: 'BUCKET-REGION'
+      bucket: 'securefilevault',
+      region: 'ap-south-1'
     }
   }
 };
